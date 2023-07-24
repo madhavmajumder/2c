e3d630b2bbcb512f28563a1edd40bf790ce177e0
madhavmajumder <107029815+madhavmajumder@users.noreply.github.com>
2023-07-24 10:13:35 +0530

    Create rating.js

---
+++ rating.js
@@ -0,0 +1,16 @@
+{
+  "@context": "https://schema.org/",
+  "@type": "Review",
+  "reviewBody": "The restaurant has great ambiance.",
+  "itemReviewed": {
+    "@type": "Restaurant",
+    "name": "Fine Dining Establishment"
+  },
+  "reviewRating": {
+    "@type": "Rating",
+    "ratingValue": 5,
+    "worstRating": 1,
+    "bestRating": 5,
+    "reviewAspect": "Ambiance"
+  }
+}
